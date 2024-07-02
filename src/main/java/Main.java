27875d2f066e7daad66c629976011da082ff7946
GALIH SAKA <galihsaka@gmail.com>
2024-07-02 16:58:42 +0700

    delete unused import at Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,7 @@
-import com.enigma.InputValidation;
 import component.Menu;
 import configuration.DIConfiguration;
-import entity.ToDo;
-import entity.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import service.ToDoService;
-import service.ToDoServiceImpl;
 
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
